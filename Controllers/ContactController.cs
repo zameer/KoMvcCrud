637c9640f0e08b3ff4c585d82aeb1a2a71d77451
sameer <sameer@jeylabs.onmicrosoft.com>
2013-05-05 17:14:28 +0530

    CRUD KO MVC4

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blogging.Web.Models;
+
+
+namespace Blogging.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /Contact/
+
+        public JsonResult Save(Blog blog)
+        {
+            int blogid = blog.BlogId;
+            BloggingContext db = new BloggingContext();
+            Blog _blg = (from b in db.Blogs
+                         where b.BlogId == blogid
+                         select b).SingleOrDefault();
+
+            if (_blg != null)
+                _blg = blog;
+            else
+                db.Blogs.Add(blog);
+
+            int ires = db.SaveChanges();
+
+            return Json(blog, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Remove(Blog blog)
+        {
+            int blogid = blog.BlogId;
+            BloggingContext db = new BloggingContext();
+            Blog _blg = (from b in db.Blogs
+                       where b.BlogId == blogid
+                       select b).SingleOrDefault();
+            db.Blogs.Remove(_blg);
+            int ires = db.SaveChanges();
+            string res = "Remove Success";
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetBlogs()
+        {
+            BloggingContext db = new BloggingContext();
+            var _blogs = from s in db.Blogs
+                        select s;
+            return Json(_blogs, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Scripts/Blog.js
@@ -0,0 +1,65 @@
+﻿
+function Blog(data) {
+    this.BlogId = ko.observable(data.BlogId);
+    this.Name = ko.observable(data.Name);
+    this.Url = ko.observable(data.Url);
+}
+
+function Post(data) {
+    this.Title = ko.observable(data.Title);
+    this.Content = ko.observable(data.Content);
+    this.BlogId = ko.observable(data.BlogId);
+}
+
+function BlogViewModel() {
+    var self = this;
+
+    self.BlogId = ko.observable();
+    self.Name = ko.observable();
+    self.Url = ko.observable();
+
+    self.Blogs = ko.observableArray([]);
+
+    self.AddBlog = function () {
+
+        var _blog = new Blog({
+            BlogId: self.BlogId(),
+            Name: self.Name(),
+            Url: self.Url()
+        });
+
+        $.post("Contact/Save", _blog, function (result) {
+            self.Blogs.push(result);
+        })
+
+        self.BlogId("");
+        self.Name("");
+        self.Url("");
+    };
+
+    self.RemoveBlog = function (blog) {
+        if (confirm('Are you sure you want to delete this ' + blog.Name() + '?')) {
+            $.post("Contact/Remove", blog, function (result) {
+                $("#message").html(blog.Name() + ' Successfully removed.');
+                $("#message").fadeIn(3000);
+                $("#message").fadeOut(3000);
+            })
+            self.Blogs.remove(blog);
+        }
+    };
+
+    self.EditBlog = function (blog) {
+        self.BlogId(blog.BlogId());
+        self.Name(blog.Name());
+        self.Url(blog.Url());
+    };
+
+    $.post("Contact/GetBlogs", function (results) {
+        var blogs = $.map(results, function (item) {
+            return new Blog(item)
+        });
+        self.Blogs(blogs);
+    })
+}
+
+ko.applyBindings(new BlogViewModel());
